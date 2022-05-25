a139bff0e09c9f097505d7d16ccb6e97660e4cd6
michalrusak <michal1406@onet.pl>
2022-05-25 20:11:16 +0200

    div zarzad

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,46 @@
 $(document).ready(function () {
   $(".my-slider").unslider();
+  window.scrollTo({top:0})//Scrollowanie strony po odswiezeniu na poczatek
 });
+//Scrollowanie scrony przez klikniecie przyciskow u gory
+function scroolHome(){
+  window.scrollTo({
+      top:60,
+      behavior:'smooth'
+  })}
+function scroolWydarzenia(){
+  window.scrollTo({
+      top:1000,
+      behavior:'smooth'
+  })}
+  function scroolCeny(){
+    window.scrollTo({
+        top:1850,
+        behavior:'smooth'
+    })}
+    function scroolKontakt(){
+      window.scrollTo({
+          top:2300,
+          behavior:'smooth'
+      })}
+    //Guzik od scrollowania strony do gory
+
+//Wywolanie funkcji pokazanai guzika po scrollowaniu strony o 20 px
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+function topFunction() {
+  window.scrollTo({
+    top:0,
+    behavior:'smooth'
+})
+  
+}
